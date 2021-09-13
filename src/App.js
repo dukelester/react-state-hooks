5adbaeafe871058a396928023fb33c73fc5cec2c
dukelester <dukelester4@gmail.com>
2021-09-13 23:55:01 +0300

    react the useState hook usage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ClassComponentCounter from "./components/ClassComponentCounter";
 import HookCounter from "./components/HookCounter";
 import HookCounter2 from "./components/HookCounter2";
 import HookCounter3 from "./components/HookCounter3";
+import HookCounter4 from "./components/HookCounter4";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <HookCounter/>
       <HookCounter2/>
       <HookCounter3/>
+      <HookCounter4/>
 
     </div>
   );
---
+++ src/components/HookCounter4.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+
+function HookCounter4 () {
+
+    const [items, setItems ]=  useState([]) //empty array destructuring
+
+    const addItem = () =>{
+        setItems([...items, {
+            id:items.length,
+            value: Math.floor(Math.random() * 10 ) + 1
+        }])
+    }
+    return (
+        <div>
+            <button onClick={addItem}>Add a number</button>
+            <ul>
+                {
+                    items.map(item =>(
+                        <li key={item.id}> {item.value}</li>
+                    ))
+
+                }
+            </ul>
+        </div>
+    )
+}
+
+
+export default HookCounter4;
